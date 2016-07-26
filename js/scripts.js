ebc34e3aabe8d855201c987147abbf6b7116713c
Amanda Daly <techdaly@gmail.com>
2016-07-25 23:13:53 -0700

    Working js menu

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var colors = ["red", "blue", "white", "yellow", "black", "green", "purple", "mulberry", "ash", "brown", "sky", "amaranth", "ermine", "vair"];
+
+var currentColor = 0;
+
+var colorLoop = function(currentColor) {
+  return colors[Math.abs(currentColor % colors.length)];
+}
+
+$(".color").addClass(colors[0]);
+
+$(".button-right").click(function(){
+  $(".color").removeClass(colorLoop(currentColor));
+  currentColor++;
+  $(".color").addClass(colorLoop(currentColor));
+})
+
+$(".button-left").click(function(){
+  if(currentColor === 0) {
+    currentColor = colors.length;
+  }
+  
+  $(".color").removeClass(colorLoop(currentColor));
+  currentColor--;
+  $(".color").addClass(colorLoop(currentColor));
+})
