af9c9b17ed85b77930c9225f5fbabade96f99abb
Amanda Daly <techdaly@gmail.com>
2016-07-26 09:45:22 -0700

    Added shape changing functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
+// FIELD COLOR /////////////////////////////////////////////////
 var colors = ["red", "blue", "white", "yellow", "black", "green", "purple", "mulberry", "ash", "brown", "sky", "amaranth", "ermine", "vair"];
-
 var currentColor = 0;
-
 var colorLoop = function(currentColor) {
   return colors[Math.abs(currentColor % colors.length)];
 }
@@ -22,4 +21,30 @@ $(".field-left").click(function(){
   $(".color").removeClass(colorLoop(currentColor));
   currentColor--;
   $(".color").addClass(colorLoop(currentColor));
+})
+
+// SHIELD SHAPE ////////////////////////////////////////////////
+
+var shapes = ["heater", "cartouche", "swiss", "iberian", "lozenge"];
+var currentShape = 0;
+var shapeLoop = function(currentShape) {
+  return shapes[Math.abs(currentShape % shapes.length)];
+}
+
+$(".shape").addClass(shapes[0]);
+
+$(".shape-right").click(function(){
+  $(".shape").removeClass(shapeLoop(currentShape));
+  currentShape++;
+  $(".shape").addClass(shapeLoop(currentShape));
+})
+
+$(".shape-left").click(function(){
+  if(currentShape === 0) {
+    currentShape = shapes.length;
+  }
+
+  $(".shape").removeClass(shapeLoop(currentShape));
+  currentShape--;
+  $(".shape").addClass(shapeLoop(currentShape));
 })
